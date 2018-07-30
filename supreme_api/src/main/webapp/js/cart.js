5d4e52d3e40dfe66a54edd5a9bc759b9f2e65b0c
linmaoshengExt <18263047573@163.com>
2018-07-30 09:33:16 +0800

    添加下单也js

--- supreme_api/src/main/webapp/js/cart.js
+++ supreme_api/src/main/webapp/js/cart.js
@@ -2,6 +2,15 @@ $(document).ready(function() {
     websqlOpenDBSUP();
     websqlCreatTable(GoodCard);
     shopTable(GoodCard);
+/*    $("#paypal-button-container").click(function(){
+     
+       if ( check()){
+    	   alert(chenggong)
+       }else {
+		   alert(fail)
+	}
+           
+    });*/
 
 });
 
@@ -22,23 +31,23 @@ function shopTable(tableName) {
                 html += "<tbody>";
                 html += "<tr>";
                 html += "<td style=\"font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif\">";
-                //图片
+             
                 html += "<img src=\"" + result.rows.item(i).goodsPicture + "\" align=\"left\" width=\"60\" height=\"60\" class=\"order-list__product-image\" style=\"border: 1px solid #e5e5e5; border-radius: 8px; margin-right: 15px\">";
                 html += "<td>";
                 html += "<td class=\"order-list__product-description-cell\" style=\"font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif; width: 100%\">";
                 html += "<span class=\"order-list__item-title\"";
-                //商品名称
-                html += "style=\"color: #555; font-size: 16px; font-weight: 600; line-height: 1.4\">" + result.rows.item(i).goodsName + "";
+             
+                html += "style=\"color: #E5E5E5	; font-size: 16px; font-weight: 600; line-height: 1.4\">" + result.rows.item(i).goodsName + "";
                 html += "</span><br> <span";
                 html += "class=\"order-list__item-variant\"";
-                //尺码
-                html += "style=\"color: #999; font-size: 14px\">" + result.rows.item(i).sizeName + "</span>";
+           
+                html += "style=\"color: #E5E5E5	; font-size: 14px\">" + result.rows.item(i).sizeName + "</span>";
                 html += "</td>";
                 html += "<td class=\"order-list__price-cell\"";
                 html += "style=\"font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', '";
                 html += "Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif; white-space: nowrap\">";
-                html += "<p class=\"order-list__item-price\" style=\"color: #555; font-size: 16px; font-weight: 600; line-height: 150%; margin: 0 0 0 15px; text-align: right\"";
-                //价钱
+                html += "<p class=\"order-list__item-price\" style=\"color: #E5E5E5; font-size: 16px; font-weight: 600; line-height: 150%; margin: 0 0 0 15px; text-align: right\"";
+              
                 html += "align=\"right\">" + result.rows.item(i).goodsPrices + "</p>";
                 html += "</td>";
                 html += "</tr>";
@@ -50,8 +59,10 @@ function shopTable(tableName) {
             }
 
             var htmlTotal = '';
-            htmlTotal += " <td class=\"subtotal-line__value\" " + "style=\"font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif; padding: 20px 0 0; " + "text-align: right\" align=\"right\"><strong  id=\"totalmoney\"style=\"color: #555; font-size: 24px\">" + totalprice + " USD</strong></td>";
-            $("#shop-cart-list").html(html) $("#total-money").html(htmlTotal) paypal1(totalprice)
+            htmlTotal += " <td class=\"subtotal-line__value\" " + "style=\"font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif; padding: 20px 0 0; " + "text-align: right\" align=\"right\"><strong  id=\"totalmoney\"style=\"color: #E5E5E5; font-size: 24px\">" + totalprice + " USD</strong></td>";
+            $("#shop-cart-list").html(html) 
+            $("#total-money").html(htmlTotal) 
+            paypal1(totalprice)
         },
         function(tx, error) {
             
@@ -61,34 +72,33 @@ function shopTable(tableName) {
 var totalcart;
 function paypal1(totalMoney) {
     totalcart = totalMoney;
-
+  
     paypal.Button.render({
-
+    	 
         env: 'sandbox',
-        // sandbox | production
+       
         style: {
             label: 'buynow',
             fundingicons: true,
-            // optional
+            
             branding: true,
-            // optional
+           
             size: 'large',
-            // small | medium | large | responsive
+            
             shape: 'rect',
-            // pill | rect
-            color: 'black' // gold | blue | silver | black
+          
+            color: 'silver' 
         },
-        // PayPal Client IDs - replace with your own
-        // Create a PayPal app: https://developer.paypal.com/developer/applications/create
+       
         client: {
             sandbox: 'AT8kaYOysDqVlmntBFjT3OoLMKY2WaxmpDoWfcHSES5vh28nyhdaoV7e8fQh-k-1nKmKJuHJHIggmFLA',
             production: ''
         },
 
-        // Show the buyer a 'Pay Now' button in the checkout flow
+       
         commit: true,
 
-        // payment() is called when the button is clicked
+       
         payment: function(data, actions) {
             return actions.payment.create({
                 payment: {
@@ -102,16 +112,60 @@ function paypal1(totalMoney) {
             });
         },
 
-        // onAuthorize() is called when the buyer approves the payment
         onAuthorize: function(data, actions) {
 
-            // Make a call to the REST api to execute the payment
             return actions.payment.execute().then(function() {
                 window.alert('Payment Complete!');
             });
         }
 
-    },
-    '#paypal-button-container');
+    },'#paypal-button-container');
 
+}
+function check() {
+	var result = true;
+	if ($("#Fname").val() == "") {
+		alert("请输入first name");
+		$("#Fname").focus(function(){
+		    $("input").css("background-color","#FF0000");
+		  });
+		return false;
+	}
+	if (isNaN($("#appId").val())) { 
+		alert("应用ID请输入数字"); 
+		$("#appId").focus();
+		return false;
+	}
+	if ($("#appId").val() == "") {
+		alert("请输入应用ID");
+		$("#appId").focus();
+		return false;
+	}
+	if ($("#appName").val() == "") {
+		alert("请输入应用名称");
+		$("#appName").focus();
+		return false;
+	}
+	if ($("#version").val() == "") {
+		alert("请输入版本");
+		$("#version").focus();
+		return false;
+	}
+	if ($("#versionInt").val() == "") {
+		alert("请填写版本INT（整数）");
+		$("#versionInt").focus();
+		return false;
+	}
+	if ($("#updatePeriod").val() == "") {
+		alert("请填写缓存更新周期");
+		$("#updatePeriod").focus();
+		return false;
+	}
+	if ($("#recommendGroupId").val() == "0") {
+		alert("请填选择推荐分组");
+		$("#recommendGroupId").focus();
+		return false;
+	}
+	
+	return result;
 }
